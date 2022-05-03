10d5fd5fe2b145a22a88f35e9b98bf81c7d5a04a
DecentralisedTech <contact@decentralised.tech>
2022-05-03 15:45:43 +1200

    Upgrade packages and static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,11 +12,17 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+    prerender: {
+      enabled: true,
+      default: true
+    },
     files: { lib: 'src/lib', assets: 'static' },
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
   }
 };
 
