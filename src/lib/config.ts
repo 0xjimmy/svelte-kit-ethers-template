57f696c31d6c65b5c23c51a5f8cbc8b58e05d735
nickmura <92566574+nickmura@users.noreply.github.com>
2022-08-02 15:44:58 -0600

    Changed rpcUrl for your own configuration.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -10,10 +10,10 @@ export type Network = {
 export const NETWORKS: Network[] = [
   {
     chainId: 1,
-    rpcUrl: 'https://eth-mainnet.alchemyapi.io/v2/52J3V2vSvpezdbzm6a7n4_VZXzCJv6r4', // Your RPC endpoint
+    rpcUrl: '', // Enter your RPC provider endpoint (Infura, Alchemy, localhost, etc)
     chainName: 'Ethereum',
     nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-    multicall2Address: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
+    multicall2Address: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696', // Address to Uniswap Multicall2 Contract
     blockExplorerUrl: 'https://etherscan.com'
   },
   // {
